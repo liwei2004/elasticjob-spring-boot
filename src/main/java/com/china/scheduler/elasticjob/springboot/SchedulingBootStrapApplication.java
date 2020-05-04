fd1eb4ee16a8ddd88c010dbab8c258635752657d
liwei <1217475244@qq.com>
2020-05-04 16:17:25 +0800

    Initial commit

---
+++ src/main/java/com/china/scheduler/elasticjob/springboot/SchedulingBootStrapApplication.java
@@ -0,0 +1,11 @@
+package com.china.scheduler.elasticjob.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingBootStrapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulingBootStrapApplication.class, args);
+    }
+}
